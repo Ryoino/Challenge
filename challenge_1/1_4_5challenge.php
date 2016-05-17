89b4e9db57d16dd40eafb7f649d84c454c49f121
井上 良太 <ryo9inoni@gmail.com>
2016-05-17 22:19:56 +0900

    修正

--- challenge_1/1_4_5challenge.php
+++ challenge_1/1_4_5challenge.php
@@ -1,13 +1,15 @@
 <?php
-$addition = 5 + 7 ; //足し算
-$subtraction = 4 - 2 ; //引き算
-$multiplication = 6 * 1 ; //掛け算
-$division = 8 / 4 ; //割り算
-$remainder = 70 % 2 ; //剰余
-echo '<h2>課題4・課題5</h2>'
-echo $addition.'<br>';
-echo $subtraction.'<br>';
-echo $multiplication.'<br>';
-echo $division.'<br>';
-echo $remainder.'<br>';
+//課題4
+$addition = 5 + 7 ;           //足し算
+$subtraction = 4 - 2 ;        //引き算
+$multiplication = 6 * 1 ;     //掛け算
+$division = 8 / 4 ;           //割り算
+$remainder = 70 % 2 ;         //剰余
+
+echo '<h2>課題5</h2><br>';
+echo '<span>5+7=</sapn>'.$addition.'<br>';
+echo '<sapn>4-2=</sapn>'.$subtraction.'<br>';
+echo '<sapn>6×1=</sapn>'.$multiplication.'<br>';
+echo '<sapn>8÷4=</sapn>'.$division.'<br>';
+echo '<sapn>70%2=</sapn>'.$remainder.'<br>';
 ?>
