e0fffd0a5ea0269fce863488806483b25deed806
井上 良太 <ryo9inoni@gmail.com>
2016-05-24 16:15:24 +0900

    プログラミング基礎2  課題：応用編

--- challenge_2/2_10challenge.php
+++ challenge_2/2_10challenge.php
@@ -1,28 +1,29 @@
-
 <?php
-
 $int = $_GET['count'];
  echo '元の値：'.$int.'<br>';
 
  if($int <= 0 || is_float($int)){
    echo '正の数ではない';
  }
-  //因数分解の処理スタート！
- else {
-        for($i = 2; true;){
-        if(($int % $i) === 0){
-            echo $i;
-            $int = $int / $i;
-        }
-     else {
-      ++$i;
-      }
 
-    if($int <= $i){
-    if($int === 1){echo '1';}
-    elseif($int == $i){ echo $i;}
-    break;
-  }
- }
+echo '<p>1ケタの素因数</p>';
+$a = 2;
+ while ($a < 10) {
+   if ($int >= $a) {
+     while(($int % $a) == 0){
+          $int /= $a;
+          echo $a.'<br>';
 }
+     $a += 1;
+   }
+ }
+echo '<p>その他</p>';
+ while ($int >= $a) {
+
+ while(($int % $a) == 0){
+      $int /= $a;
+      echo $a.'<br>';
+    }
+    $a++;
+  }
 ?>
